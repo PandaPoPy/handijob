7bb54ffec59f8542c722122b8b5c25a05f37d218
Vincent <vlhoste@thinkcode.co>
2017-11-28 11:23:59 +0100

    Add foreignkey Offer-Enterprise

--- thinkjob/offers/admin.py
+++ thinkjob/offers/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from offers.models import Offer
+from offers.models import Offer, Enterprise
 
 
 class OfferAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Offer, OfferAdmin)
+
+
+class EnterpriseAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Enterprise, EnterpriseAdmin)
--- thinkjob/offers/models.py
+++ thinkjob/offers/models.py
@@ -1,10 +1,12 @@
 from django.core.urlresolvers import reverse
 from django.db import models
 
+from candidateuser.models import User
+
 
 class Enterprise(models.Model):
     #logo=models.ImageField()
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     address = models.CharField(max_length=300)
     postcode = models.CharField(max_length=50)
     city=models.CharField(max_length=100)
@@ -12,12 +14,15 @@ class Enterprise(models.Model):
     def get_absolute_url(self):
         return reverse('enterprise_detail', args=(self.id,))
 
+    def __str__(self):
+        return self.name
+
 
 class Offer(models.Model):
     title=models.CharField(max_length=200)
-    description=models.TextField(max_length=400)
-    profile_researched=models.TextField(max_length=400)
-    enterprise = models.ForeignKey(Enterprise)
+    description=models.TextField(max_length=400, blank=True)
+    profile_researched=models.TextField(max_length=400, blank=True)
+    enterprise = models.ForeignKey('Enterprise')
 
     def get_absolute_url(self):
         return reverse('offer_detail', args=(self.id,))
@@ -25,4 +30,12 @@ class Offer(models.Model):
     def __str__(self):
         return self.title
 
-
+#
+# class Appliance(models.Model):
+#     candidate_user = models.ForeignKey(User)
+#     offer= models.ForeignKey('Offer')
+#
+#     class Meta:
+#         unique_together=(  # cela va permettre une contrainte d'unicité pour empêcher d'avoir 2 même candidatures pour un même Candidat
+#             ('candidate_user', 'offer'),
+#         )
